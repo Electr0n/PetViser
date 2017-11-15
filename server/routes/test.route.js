5e46654050ba371d6068dbc15d066f6a2546f5f8
gimly93 <a.lukashevich93@gmail.com>
2017-11-15 20:46:12 +0300

    node server with express. with simple '/api/status' route for testing

--- server/routes/test.route.js
+++ server/routes/test.route.js
@@ -1,5 +1,5 @@
-module.exports = (app) =>{
-  app.get('/api/status',(req,res)=>{
+module.exports = (app) => {
+  app.get('/api/status', (req, res) => {
     res.send('Welcome to Pet adviser');
   });
 };
